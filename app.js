20babcf66ea14fed69c4c41676c7720e5455d981
thamiscoder <thamirisoli34@gmail.com>
2025-02-14 21:36:36 -0300

    desafio-alura01

---
+++ app.js
@@ -0,0 +1,28 @@
+//alert('Boas Vindas!');
+
+//let nome = 'Lua';
+//let idade = 25;
+//let numeroDeVendas = 50;
+//let saldoDisponivel = 1000;
+//let menssagemDeErro = '[ERRO] Preencha todos os campos!';
+
+//let nome = prompt('Digite seu nome? ');
+
+
+let idade = prompt('Digite sua idade: ');
+
+if (idade >= 18) {
+    alert(`Parabéns agora você já pode tirar sua habilitação ;)`);
+} else {
+    alert(`Você infelizmente ainda não pode tirar a habilitação :/`)
+}
+
+
+/*let idade = prompt('Digite a sua idade');
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!'); 
+}*/
+
+
+//alert(menssagemDeErro);
+//alert('[ERRO] Preencha todos os campos!')
